45fcecbcd2a40979e65f673fae01b48e9490c3e0
Pratheeksha Naresh <53502770+nkprathi@users.noreply.github.com>
2023-05-09 00:21:29 +0530

    Add files via upload

---
+++ ruby exercise/ex19_studydrill.rb
@@ -0,0 +1,21 @@
+def student_count(boys_count,girls_count)
+  puts "This school has #{boys_count} boys."
+  puts "This school has #{girls_count} girls."
+  end
+
+#type1
+student_count(20,25)
+
+#type2
+number_of_boys = 10       #using variables
+number_of_girls = 50
+
+student_count(number_of_boys,number_of_girls)
+
+
+#type3
+student_count(10 + 50, 50 + 26)
+
+
+#type4
+student_count(number_of_boys + 100, number_of_girls + 200)
